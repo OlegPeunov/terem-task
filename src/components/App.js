9890796f340c6a3606398a31b2d2902b45d6d0a6
Oleg Peunov <olegpeunov@yandex.ru>
2022-01-13 12:27:01 +0300

    basic structure

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <p>Привет мир</p>
+    </div>
+  );
+}
+
+export default App;
